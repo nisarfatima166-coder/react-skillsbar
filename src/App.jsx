4aa94c5b0b5b9c9485e212420fb6ce1ba73e4e77
nisarfatima166-coder <nisarfatima166@gmail.com>
2026-03-30 22:23:37 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+export default function App() {
+  return (
+    <>
+    <div class="container">
+    <div class="skill">
+        <div class="title">
+            <span>HTML</span>
+        </div>
+        <div class="progress">
+            <div class="progress-bar html">
+                <span class="percent">90%</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="skill">
+        <div class="title">
+            <span>CSS</span>
+        </div>
+        <div class="progress">
+            <div class="progress-bar css">
+                <span class="percent">70%</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="skill">
+        <div class="title">
+            <span>JavaScript</span>
+        </div>
+        <div class="progress">
+            <div class="progress-bar js">
+                <span class="percent">50%</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="skill">
+        <div class="title">
+            <span>NodeJS</span>
+        </div>
+        <div class="progress">
+            <div class="progress-bar node">
+                <span class="percent">30%</span>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+    
+    </>
+  );
+}
